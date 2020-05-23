967cc21c8c80edb237c3504ed97f803f3bcdd838
Albert Saenz Aragall <asaenz@edu.tecnocampus.cat>
2020-05-23 18:33:41 +0200

    Configuració Aplicació

--- src/CapaSingleton/ConfiguracioAplicacio.java
+++ src/CapaSingleton/ConfiguracioAplicacio.java
@@ -13,16 +13,24 @@ public class ConfiguracioAplicacio {
 
 	private ConfiguracioAplicacio() throws Exception {
 		try {
-			// Per implementar: Obrir i carregar el fitxer de propietats
+			FileInputStream f= new FileInputStream(NOM_FITXER_EXTERN);
+			propietatsAplicacio= new Properties();
+			propietatsAplicacio.load(f);
+			f.close();
 		} catch (IOException e) {
 			throw new Exception("No es pot carregar la Configuració del Sistema: "
 							+ e.getMessage());
 		}
 	}
-	
-	// Per implementar getInstancia del patró singleton
+
+	public static ConfiguracioAplicacio getInstancia() throws Exception {
+		if(instancia==null)
+			instancia=new ConfiguracioAplicacio();
+		
+		return instancia;
+	}
 
 	public String getConfiguracioAplicacio(String clau) {
-		// Per implementar retornar el valor de la propietat
+		return propietatsAplicacio.getProperty(clau);
 	}
 }
